98355c510b462b31bf66987d01768c20ced63ffb
Austin Hunt <austin.hunt.012@gmail.com>
2022-10-05 15:57:57 -0500

    updates

--- js/intro-js.js
+++ js/intro-js.js
@@ -1,2 +1,19 @@
-let greeting= "Hello from another external JS file";
-console.log(greeting)
+"use strict";
+
+let greeting = "Hello from another external JS file";
+console.log(greeting);
+
+// Without "Use strict" will appear fine, With "Use strict" will appear red.
+// myAge = 30
+
+//alert("You've been hacked!");
+let username= prompt("What is your name?");
+//console.log(username);
+
+//let isFull = confirm("Is the class full?");
+//console.log(isFull);
+
+alert("Hello " + username + " Welcome to my website!");
+
+
+

42d76d7f0bccb5809bf3f4f9c9791d084336afbe
Austin Hunt <austin.hunt.012@gmail.com>
2022-10-06 11:23:09 -0500

    daily update

---
+++ function_lec.js
@@ -0,0 +1,52 @@
+"use strict"
+//functions lecture and testing.
+
+//function(argument)
+//somefuntctions can accept a number of arguments
+
+//some arguments are
+//prompt(whats your name)(optional)
+
+//some are meant to store variables
+//let littleMermaid = prompt("how many days did you rent this movie?")
+
+// execute functions - definition vs execution
+
+//without partenthsis it will show the functions definition.
+//console.log(parseInt)
+
+//with parenthisis it will execute the function.
+//console.log(parseInt("42"))
+
+// function add(num1, num2) {
+// return num1 + num2
+//}
+
+//function add(num1, num2) => num1 +num2
+//both function the same way => is acting as a return and generally used   instance where the function is doing one specific task.
+
+// ================ !! Mini-exercises !!
+
+// Write a function, returnFive, that returns the number five. No inputs should be defined.
+
+
+
+// Write a function, isFive, that takes in an input and returns the boolean value true if the passed argument is the number 5 or the string "5". Return false otherwise.
+
+// how many inputs should the function have?
+// what are the data types for the inputs?
+// what is the data type of the output/return value?
+
+
+// Write a function, isShortWord, that takes in a string and returns the boolean value true if the passed argument is shorter than 5 characters. Return false otherwise.
+
+// how many inputs?
+// what data type for inputs?
+// what is the return type?
+
+
+// Write a function, isSameLength, that takes in two string inputs and returns the boolean value true if the passed arguments are the same length. Return false otherwise.
+
+// How many inputs?
+// What type of input?
+// What type of output?

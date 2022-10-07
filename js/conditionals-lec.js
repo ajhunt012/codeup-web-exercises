15f0a674219c224bc11777629069a2173f30c17d
Austin Hunt <austin.hunt.012@gmail.com>
2022-10-07 15:34:26 -0500

    updates

--- js/conditionals-lec.js
+++ js/conditionals-lec.js
@@ -1,5 +1,5 @@
 "use strict";
-(function(){
+(function() {
 
     // if Statement - allows you to execute code based on certain conditions.
     // if conditions are not met then the code in the curly braces will not execute.
@@ -12,7 +12,7 @@
 
     // if / else statements - in addition to the if statement, the else will block will run if the condition is not met
 
-    if(true) {
+    if (true) {
         // condition evaluates to true, run this code
         console.log("the code evaluates to true");
     } else {
@@ -23,7 +23,7 @@
     let currentShow = prompt('What show are you currently watching?')
     // the .lowerCase will help rid of uppercase issues to titles
 
-    if(currentShow.toLowerCase() === "House of Dragons") {
+    if (currentShow.toLowerCase() === "House of Dragons") {
         console.log("I love that show!");
     } else {
         console.log("I havent seen that show just yet.");
@@ -43,7 +43,7 @@
     let userUsername = prompt("What is your username?");
     let userPassword = prompt("What is your password?");
 
-    if((password === userPassword) && (username === userUsername)) {
+    if ((password === userPassword) && (username === userUsername)) {
         console.log("Successfully logged in.");
     } else {
         console.log("User credentials are incorrect!")
@@ -72,7 +72,7 @@
         console.log('you\'re dismissed.');
     } else if (jeans === "denim") {
         console.log('you are dismissed');
-    }  else if (jacket) {
+    } else if (jacket) {
         console.log("take of the jacket and go.");
     } else {
         console.log("Everyone else get out.");
@@ -81,22 +81,65 @@
 
     let num = 10
 
-    if(num % 2 === 0 && num % 5 === 0)
+    if (num % 2 === 0 && num % 5 === 0)
 
-        let myName = "Austin";
 // do not use one of these if you will do more than one line or one else of code.
 
     let myName = "Austin" ? myName === 'Austin' ? "Hello," + myName : "Hello, Stranger!";
 
-     // Is the same as
+    // Is the same as
     if (myName === 'Austin') {
         console.log("Hello," + myName);
     } else {
         console.log("Hello ,Stranger.");
     }
 
+// switch statements
+
+    let menu = prompt('1. cheesburger \n2. chicken sandwich \n3. chilli cheese fries\n4. philly cheesesteak');
+
+    switch (menu) {
+        case '1':
+            alert("The cheeseburger combo is $5.99")
+            break;
+        case '2':
+            alert("The chicken sandwich is freshly made, and cost $4.99.")
+            break;
+        case '3':
+            alert("The chilli cheese fries, come with jalapenos and cost $3.99.")
+            break;
+        case '4':
+            alert("The philly cheesesteak is done the 'Texas Way' and is $7.99.")
+            break;
+        default:
+            alert("I'm Sorry that is not a menu item.");
+
+        // Without the breaks the code will run and give all results to the intended function or user.
+
+    }
+
+    // let menu
+    // +prompt('1. cheesburger \n2. chicken sandwich \n3. chilli cheese fries\n4. philly cheesesteak');
+    // if (menu === '1') {
+    //     alert("The cheeseburger combo is $5.99")
+    // }
+    // if else
+    // (menu === '2')
+    // {
+    //     alert("The chicken sandwich is freshly made, and cost $4.99.");
+    // }
+    // if else
+    // (menu === '3')
+    // {
+    //     alert("The chilli cheese fries, come with jalapenos and cost $3.99.")
+    // }
+    // if else
+    // (menu === '4')
+    // {
+    //     alert("The philly cheesesteak is done the 'Texas Way' and is $7.99.")
+    // }else {
+    //     alert("I'm Sorry that is not a menu item.");
+    // }
 
 
-
-
-})();
+    })();

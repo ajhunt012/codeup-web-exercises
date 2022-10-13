84d8d2bdda032ae254677b4c863bbe20f7a7f0a5
Austin Hunt <austin.hunt.012@gmail.com>
2022-10-13 16:30:30 -0500

    finished for loops

---
+++ js/for_loops.js
@@ -0,0 +1,46 @@
+(function() {
+    "use strict";
+
+    function showMultiplicationTable(num) {
+        console.log(num * 2)
+        console.log(num * 3)
+        console.log(num * 4)
+        console.log(num * 5)
+        console.log(num * 6)
+        console.log(num * 7)
+        console.log(num * 8)
+        console.log(num * 9)
+        console.log(num * 10)
+    }
+    showMultiplicationTable(7);
+
+
+        for (let x=1; x<=10; x++) {
+            let random = Math.floor(Math.random() * 200) + 20;
+            if (random % 2 == 0) {
+                console.log(random + " This number is even");
+            }
+            else {
+                console.log(random + " This number is odd");
+            }
+        }
+
+
+        for (let i=1; i<=9; i++){
+            let pyramid = "";
+            for (let j=1; j<=i; j++) {
+                pyramid+=i
+            }
+            console.log(pyramid)
+        }
+
+
+        for (let i=100; i>=5; i-=5) {
+            console.log(i);
+        }
+
+})();
+
+
+
+

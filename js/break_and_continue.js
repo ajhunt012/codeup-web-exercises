8b56d17d70bf609934d9d8808f48b51056d1ccbe
Austin Hunt <austin.hunt.012@gmail.com>
2022-10-14 09:36:13 -0500

    started/finished while.js

---
+++ js/break_and_continue.js
@@ -0,0 +1,20 @@
+(function(){
+    "use strict";
+
+    //Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
+// Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+
+    let userNumber = parseFloat(prompt('Please enter a number between 1 and 50.'));
+
+    for (let i = 0; i < 50; i++ );{
+        if (userNumber !== NaN && userNumber <50 && userNumber > 1); {
+            break; }
+        if (i === userNumber) {
+            continue;
+        }
+        if (i % 2 !== 0) {
+            console.log(i + " This is an odd number!")
+        }
+    }
+
+})();

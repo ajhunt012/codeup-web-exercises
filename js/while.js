35e5c64dc5f65d5a50433b3bb7f48d3026c4078a
Austin Hunt <austin.hunt.012@gmail.com>
2022-10-14 11:48:12 -0500

    updates

--- js/while.js
+++ js/while.js
@@ -1,8 +1,20 @@
+(function(){
+    "use strict";
+
 
 
 let i = 2;
 
 while (i < 65537) {
     console.log(i);
-    i= i*2;
+    i = i * 2;
 }
+
+
+let num = 1
+
+while(num < 65536){
+    console.log(num *= 2);
+}
+
+})();

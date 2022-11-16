924642b1a05bbd7729a1dd485345a9f6e20f315b
Austin Hunt <austin.hunt.012@gmail.com>
2022-11-16 11:59:11 -0600

    finished mouse selectors

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,11 +1,45 @@
 "use strict";
-(function (){
+(function(){
+    $(document).ready(function() {
+
+
+       //  let h1Alert = $('#first-h1').html();
+       // alert(h1Alert);
+
+        // let h1Alert = $('#first-p').html();
+        // alert(h1Alert);
+
+
+       // $('.codeup').css('border', '1px solid red');
+
+
+        // $('.codeup').css('font-size', '20px');
+        // $('.codeup').css('background-color', 'green');
+        // $('.paragraph').css('background-color', 'yellow');
+
+        // let h1Alert = $('#first-h1').html();
+        // alert(h1Alert);
+
+        // $('.codeup, .title-card, .paragraph').css('font-size', '20px');
+
+        // $('#first-h1').click(function(event) {
+        //     $('#first-h1').css('background-color', 'yellow');
+        // });
+
+        // $('.paragraph').dblclick(function(event) {
+        //     $('.paragraph').css('font-size', '18px');
+        // });
+
+        $('.codeup').hover(
+            function() {
+                $(this).css('color', 'red');
+            },
+            function() {
+                $(this).css('color', 'black');
+            }
+        );
 
 
 
-$(function() {
-    alert( 'The DOM has finished loading!' );
-});
 
-
-})();
+    })})();

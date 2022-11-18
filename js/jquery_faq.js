f1774e64a751017a56c2c6fb84b45d1ebcddb8eb
Austin Hunt <austin.hunt.012@gmail.com>
2022-11-18 09:38:08 -0600

    finished exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -7,10 +7,10 @@
         // });
 
 
-  $('.toggle').click(function (e){
-      e.preventDefault();
-      $('dd').toggleClass('invisible');
-  })
+        $('.toggle').click(function (e) {
+            e.preventDefault();
+            $('dd').toggleClass('invisible');
+        })
 
 
         $('dt').click(function () {
@@ -26,6 +26,40 @@
         //     e.preventDefault();
         //     $(this).css('background-color', 'yellow');
         // })
-s
+
+        // We could use jQuery to highlight every other li element using the .each() method:
+
+        // $('li').each(function(index) {
+        //     if (index % 2 !== 0) {
+        //         $(this).css('background-color', '#FF0');
+        //     }
+        // });
+
+        // $('.parksBtn').click(function (){
+        // $('li').last().css('background-color', 'yellow');
+        // });
+
+        $('.parksBtn').click(function (e) {
+            e.preventDefault();
+            $('ul').each(function () {
+                $(this).children().last().css('background-color', 'yellow')
+            })
+        });
+
+        // anytime an h3 is clicked, bolds all the list items under
+        $('h3').click(function () {
+            $(this).next().css('font-weight', 'bold');
+        })
+
+
+        // $('each').click(function (e) {
+        //     e.preventDefault();
+        //     $('ul').each(function () {
+        //         $(this).parent().first().css('color', 'blue')
+        //     })
+        // });
+
+
     })
+
 })();
